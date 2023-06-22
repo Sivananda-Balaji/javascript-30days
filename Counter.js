276501ee7ec6272a425857b31f4c02f0ccf62061
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-06-22 11:37:20 +0530

    add counter solution

---
+++ Counter.js
@@ -0,0 +1,12 @@
+//2620. Counter
+
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
