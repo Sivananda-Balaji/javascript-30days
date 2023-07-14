31fcb48a694811fc71eca51fd36cedb64e38bb53
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-14 09:16:50 +0530

    add calculator solution

---
+++ Calculator.js
@@ -0,0 +1,43 @@
+//2726. Calculator with Method Chaining
+
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    } else {
+      this.result /= value;
+      return this;
+    }
+  }
+
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
+
+const result = new Calculator(10).add(5).subtract(7).getResult();
+console.log(result);
